84ad79fbf5c799ba658653ab16b15bf16ac05d6f
唐鑫 <tangx@huayingmedia.com>
2022-07-18 14:28:15 +0800

    feat: 转化成 kts, 发布至 Maven 相关设置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,29 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    
+
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.0'
-        classpath 'com.github.dcendents:android-maven-gradle-plugin:1.5'
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.21.0")
     }
 }
 
 allprojects {
     repositories {
         google()
-        jcenter()
-        maven { url 'https://jitpack.io' }
+        mavenCentral()
+        maven("https://www.jitpack.io")
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
